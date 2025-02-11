6dd689c0173138b182c62132a86fd80252e3dd6c
PhiShora <125209612+PhiShora@users.noreply.github.com>
2025-02-11 15:38:57 -0300

    Update crud_time.py

--- crud_time.py
+++ crud_time.py
@@ -1,58 +1,62 @@
-# Projeto_BD - CRUD
-
 from database import conecta_bd, encerra_conn
 
+class CRUDTime:
+    def __init__(self):
+        self.connection = conecta_bd()
+        self.cursor = self.connection.cursor()
 
-def main():
-    connection = conecta_bd()
-    cursor = connection.cursor()
+    def inserir(self, sigla, nome_clube, fundacao, mascote, n_titulos):
+        try:
+            cmd_insert = """
+                INSERT INTO time (sigla, nome_clube, fundacao, mascote, n_titulos)
+                VALUES (%s, %s, %s, %s, %s)
+            """
+            valores = (sigla, nome_clube, fundacao, mascote, n_titulos)
+            self.cursor.execute(cmd_insert, valores)
+            self.connection.commit()
+            return "Dados inseridos com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao inserir dados: {err}"
 
-    # CREATE
-    def inserir(sigla, nome_clube, fundacao, mascote, n_titulos):
-        cmd_insert = "INSERT INTO time (sigla, nome_clube,fundacao,mascote,n_titulos) VALUES (%s,%s,%s,%s,%s)"
-        valores = (sigla, nome_clube, fundacao, mascote, n_titulos)
-        cursor.execute(cmd_insert, valores)
-        connection.commit()
-        print("Dados inseridos com sucesso!!")
+    def selecionar(self, sigla=None, nome_clube=None):
+        try:
+            cmd_select = "SELECT * FROM time"
+            if sigla or nome_clube:
+                cmd_select += " WHERE "
+                conditions = []
+                if sigla:
+                    conditions.append(f"sigla = %s")
+                if nome_clube:
+                    conditions.append(f"nome_clube = %s")
+                cmd_select += " AND ".join(conditions)
+            valores = tuple(filter(None, [sigla, nome_clube]))
+            self.cursor.execute(cmd_select, valores)
+            return self.cursor.fetchall()
+        except psycopg2.Error as err:
+            return f"Erro ao selecionar dados: {err}"
 
-    # READ
+    def atualizar(self, sigla, nome_clube, mascote, n_titulos, nome_original):
+        try:
+            cmd_update = """
+                UPDATE time
+                SET sigla = %s, nome_clube = %s, mascote = %s, n_titulos = %s
+                WHERE nome_clube = %s
+            """
+            valores = (sigla, nome_clube, mascote, n_titulos, nome_original)
+            self.cursor.execute(cmd_update, valores)
+            self.connection.commit()
+            return "Dados atualizados com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao atualizar dados: {err}"
 
-    def seleciona(sigla, nome_clube):
+    def deletar(self, nome_clube):
+        try:
+            cmd_delete = "DELETE FROM time WHERE nome_clube = %s"
+            self.cursor.execute(cmd_delete, (nome_clube,))
+            self.connection.commit()
+            return "Dados deletados com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao deletar dados: {err}"
 
-        cria = "SELECT * FROM time;"
-        values = sigla, nome_clube
-        cursor.execute(cria, values)
-        acoes = cursor.fetchall()
-        for acao in acoes:
-            print(acao)
-        return acao
-
-    # UPDATE
-    def atualiza(sigla, nome_clube, mascote, n_titulos, nome_original):
-        cmd_update = f'UPDATE time SET sigla = %s, nome_clube = %s, mascote = %s, n_titulos = %s WHERE nome_clube= %s'
-        valores = (sigla, nome_clube, mascote, n_titulos, nome_original)
-        cursor.execute(cmd_update, valores)
-        connection.commit()
-
-
-    # DELETE
-
-    def deleta(nome_clube):
-        cmd_delete = f"DELETE FROM time WHERE nome_clube= %s"
-        cursor.execute(cmd_delete, (nome_clube,))
-        connection.commit()
-        cursor.execute("SELECT * FROM time")
-        registros = cursor.fetchall()
-        print("\nRegistros restantes na tabela:")
-        for registro in registros:
-            print(registro)
-        return registros
-
-    # inserir("FLA","Flamengo","1895-11-05","Urubu",50)
-    #seleciona(sigla="FLA", nome_clube="Flamengo")
-    #atualiza("FLA", "Flamenga", "Macaco", 10, "Flamengo")
-    # deleta(nome_clube="Flamengo")
-
-
-if __name__ == "__main__":
-    main()
+    def __del__(self):
+        encerra_conn(self.connection)
