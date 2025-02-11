4bb6c2bdfa4f579b9687e6524e84b3ff320c611a
PhiShora <125209612+PhiShora@users.noreply.github.com>
2025-02-11 15:37:55 -0300

    Update crud_funcionarios.py

--- crud_funcionarios.py
+++ crud_funcionarios.py
@@ -1,58 +1,62 @@
-# Projeto_BD - CRUD
-
 from database import conecta_bd, encerra_conn
 
+class CRUDFuncionario:
+    def __init__(self):
+        self.connection = conecta_bd()
+        self.cursor = self.connection.cursor()
 
-def main():
-    connection = conecta_bd()
-    cursor = connection.cursor()
+    def inserir(self, matricula, nome_funcionario, idade, profissao, sigla_dep):
+        try:
+            cmd_insert = """
+                INSERT INTO funcionarios (matricula, nome_funcionario, idade, profissao, sigla_dep)
+                VALUES (%s, %s, %s, %s, %s)
+            """
+            valores = (matricula, nome_funcionario, idade, profissao, sigla_dep)
+            self.cursor.execute(cmd_insert, valores)
+            self.connection.commit()
+            return "Dados inseridos com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao inserir dados: {err}"
 
-    # CREATE
-    def inserir(matricula, nome_funcionario, idade,profissao,sigla_dep):
-        cmd_insert = "INSERT INTO funcionarios (matricula, nome_funcionario, idade,profissao,sigla_dep) VALUES (%s,%s,%s,%s,%s)"
-        valores = (matricula, nome_funcionario, idade,profissao,sigla_dep)
-        cursor.execute(cmd_insert, valores)
-        connection.commit()
-        print("Dados inseridos com sucesso!!")
+    def selecionar(self, matricula=None, nome_funcionario=None):
+        try:
+            cmd_select = "SELECT * FROM funcionarios"
+            if matricula or nome_funcionario:
+                cmd_select += " WHERE "
+                conditions = []
+                if matricula:
+                    conditions.append(f"matricula = %s")
+                if nome_funcionario:
+                    conditions.append(f"nome_funcionario = %s")
+                cmd_select += " AND ".join(conditions)
+            valores = tuple(filter(None, [matricula, nome_funcionario]))
+            self.cursor.execute(cmd_select, valores)
+            return self.cursor.fetchall()
+        except psycopg2.Error as err:
+            return f"Erro ao selecionar dados: {err}"
 
-    # READ
+    def atualizar(self, matricula, nome_funcionario, idade, profissao, sigla_dep_antiga):
+        try:
+            cmd_update = """
+                UPDATE funcionarios
+                SET matricula = %s, nome_funcionario = %s, idade = %s, profissao = %s
+                WHERE sigla_dep = %s
+            """
+            valores = (matricula, nome_funcionario, idade, profissao, sigla_dep_antiga)
+            self.cursor.execute(cmd_update, valores)
+            self.connection.commit()
+            return "Dados atualizados com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao atualizar dados: {err}"
 
-    def seleciona(matricula, nome_funcionario):
+    def deletar(self, nome_funcionario):
+        try:
+            cmd_delete = "DELETE FROM funcionarios WHERE nome_funcionario = %s"
+            self.cursor.execute(cmd_delete, (nome_funcionario,))
+            self.connection.commit()
+            return "Dados deletados com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao deletar dados: {err}"
 
-        cria = "SELECT * FROM funcionarios;"
-        values = matricula, nome_funcionario
-        cursor.execute(cria, values)
-        acoes = cursor.fetchall()
-        for acao in acoes:
-            print(acao)
-        return acao
-
-    # UPDATE
-    def atualiza(matricula, nome_funcionario, idade,profissao,sigla_dep_antiga):
-        cmd_update = f'UPDATE funcionarios SET matricula = %s, nome_funcionario = %s, idade = %s, profissao = %s WHERE sigla_dep= %s'
-        valores = (matricula, nome_funcionario, idade,profissao,sigla_dep_antiga)
-        cursor.execute(cmd_update, valores)
-        connection.commit()
-
-
-    # DELETE
-
-    def deleta(nome_funcionario):
-        cmd_delete = f"DELETE FROM time WHERE nome_clube= %s"
-        cursor.execute(cmd_delete, (nome_funcionario,))
-        connection.commit()
-        cursor.execute("SELECT * FROM time")
-        registros = cursor.fetchall()
-        print("\nRegistros restantes na tabela:")
-        for registro in registros:
-            print(registro)
-        return registros
-
-    # inserir("FLA","Flamengo","1895-11-05","Urubu",50)
-    #seleciona(sigla="FLA", nome_clube="Flamengo")
-    #atualiza("FLA", "Flamenga", "Macaco", 10, "Flamengo")
-    # deleta(nome_clube="Flamengo")
-
-
-if __name__ == "__main__":
-    main()
+    def __del__(self):
+        encerra_conn(self.connection)
