44aa1bc0975514d121f198beb3c08fff61e012a0
PhiShora <125209612+PhiShora@users.noreply.github.com>
2025-02-11 15:36:16 -0300

    Update crud_elenco.py

--- crud_elenco.py
+++ crud_elenco.py
@@ -1,58 +1,62 @@
-# Projeto_BD - CRUD
-
 from database import conecta_bd, encerra_conn
 
+class CRUDElenco:
+    def __init__(self):
+        self.connection = conecta_bd()
+        self.cursor = self.connection.cursor()
 
-def main():
-    connection = conecta_bd()
-    cursor = connection.cursor()
+    def inserir(self, id_jogador, nome_jogador, idade, valor_mercado, sigla, sigla_posicao):
+        try:
+            cmd_insert = """
+                INSERT INTO elenco (id_jogador, nome_jogador, idade, valor_mercado, sigla, sigla_posicao)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            valores = (id_jogador, nome_jogador, idade, valor_mercado, sigla, sigla_posicao)
+            self.cursor.execute(cmd_insert, valores)
+            self.connection.commit()
+            return "Dados inseridos com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao inserir dados: {err}"
 
-    # CREATE
-    def inserir(id_jogador, nome_jogador, idade,valor_mercado,sigla,sigla_posicao):
-        cmd_insert = "INSERT INTO elenco (id_jogador, nome_jogador, idade,valor_mercado,sigla,sigla_posicao) VALUES (%s,%s,%s,%s,%s,%s)"
-        valores = (id_jogador, nome_jogador, idade,valor_mercado,sigla,sigla_posicao)
-        cursor.execute(cmd_insert, valores)
-        connection.commit()
-        print("Dados inseridos com sucesso!!")
+    def selecionar(self, nome_jogador=None, sigla_posicao=None):
+        try:
+            cmd_select = "SELECT * FROM elenco"
+            if nome_jogador or sigla_posicao:
+                cmd_select += " WHERE "
+                conditions = []
+                if nome_jogador:
+                    conditions.append(f"nome_jogador = %s")
+                if sigla_posicao:
+                    conditions.append(f"sigla_posicao = %s")
+                cmd_select += " AND ".join(conditions)
+            valores = tuple(filter(None, [nome_jogador, sigla_posicao]))
+            self.cursor.execute(cmd_select, valores)
+            return self.cursor.fetchall()
+        except psycopg2.Error as err:
+            return f"Erro ao selecionar dados: {err}"
 
-    # READ
+    def atualizar(self, id_jogador, nome_jogador, idade, valor_mercado, sigla, sigla_posicao):
+        try:
+            cmd_update = """
+                UPDATE elenco
+                SET nome_jogador = %s, idade = %s, valor_mercado = %s, sigla = %s, sigla_posicao = %s
+                WHERE id_jogador = %s
+            """
+            valores = (nome_jogador, idade, valor_mercado, sigla, sigla_posicao, id_jogador)
+            self.cursor.execute(cmd_update, valores)
+            self.connection.commit()
+            return "Dados atualizados com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao atualizar dados: {err}"
 
-    def seleciona(nome_jogador,sigla_posicao):
+    def deletar(self, nome_jogador):
+        try:
+            cmd_delete = "DELETE FROM elenco WHERE nome_jogador = %s"
+            self.cursor.execute(cmd_delete, (nome_jogador,))
+            self.connection.commit()
+            return "Dados deletados com sucesso!"
+        except psycopg2.Error as err:
+            return f"Erro ao deletar dados: {err}"
 
-        cria = "SELECT * FROM elenco;"
-        values = nome_jogador,sigla_posicao
-        cursor.execute(cria, values)
-        acoes = cursor.fetchall()
-        for acao in acoes:
-            print(acao)
-        return acao
-
-    # UPDATE
-    def atualiza(id_jogador, nome_jogador, idade,valor_mercado,sigla,sigla_posicao):
-        cmd_update = f'UPDATE elenco SET id_jogador = %s, nome_jogador = %s, idade = %s, valor_mercado = %s WHERE nome_jogador= %s'
-        valores = (id_jogador, nome_jogador, idade,valor_mercado,sigla,sigla_posicao)
-        cursor.execute(cmd_update, valores)
-        connection.commit()
-
-
-    # DELETE
-
-    def deleta(nome_jogador):
-        cmd_delete = f"DELETE FROM elenco WHERE nome_clube= %s"
-        cursor.execute(cmd_delete, (nome_jogador,))
-        connection.commit()
-        cursor.execute("SELECT * FROM elenco")
-        registros = cursor.fetchall()
-        print("\nRegistros restantes na tabela:")
-        for registro in registros:
-            print(registro)
-        return registros
-
-    # inserir("FLA","Flamengo","1895-11-05","Urubu",50)
-    #seleciona(sigla="FLA", nome_clube="Flamengo")
-    #atualiza("FLA", "Flamenga", "Macaco", 10, "Flamengo")
-    # deleta(nome_clube="Flamengo")
-
-
-if __name__ == "__main__":
-    main()
+    def __del__(self):
+        encerra_conn(self.connection)
